aa7892314171360a051bcce975d339dbce57d9e9
Zababurin <zababurinsv@vk.com>
2024-04-17 00:41:31 +0300

    update

--- docs/services/test/src/component/nk-test/controller/addEventListener/index.mjs
+++ docs/services/test/src/component/nk-test/controller/addEventListener/index.mjs
@@ -3,10 +3,10 @@ export default async (self, actions) => {
     const solvMassives = self.shadowRoot.querySelector('.solv-massives')
 
     return {
-        init: () => {
+        init: async () => {
             solvMassives.addEventListener("click", actions.solvMassives);
         },
-        terminate: () => {
+        terminate: async () => {
             solvMassives.removeEventListener('click', actions.solvMassives);
         }
     }
